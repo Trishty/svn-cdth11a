9471ddf9fb37baa9d78710ac02f7850aacb6e196
hoangnam30593@gmail.com <hoangnam30593@gmail.com@fdf90f2a-8789-9f7b-c1d0-b6fda4332c5d>
2013-12-07 15:33:35 +0000

    Hoàn thành code xử lý màn hình tra cứu phòng

---
+++ Source/DoAnLon/DoAnCNPM/DAO/NhanVienDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+
+namespace DAO
+{
+    public class NhanVienDAO
+    {
+        public static DataSet HienThiDanhSachSinhVien(NhanVienDTO NV_DTO)
+        {
+            SqlConnection con = DataProvider.ConnectionString();
+            string strSQL = "select * from NhanVien";
+            return DataProvider.GetDataSet(strSQL, con);
+        }
+
+        public static bool ThemNhanVien(NhanVienDTO NV_DTO)
+        {
+            SqlConnection con = DataProvider.ConnectionString();
+
+            string strSQL = "insert into NhanVien values ('"
+            + NV_DTO.StrTenDangNhap + "','"
+            + NV_DTO.StrMatKhau + "','"
+            + NV_DTO.StrHoTen + "','"
+            + NV_DTO.StrChucVu + "','"
+            + NV_DTO.StrDiaChi + "','"
+            + NV_DTO.StrDienThoai + "','"
+            + NV_DTO.StrCMND + "')";
+
+            return DataProvider.ExecuteNonQuery(strSQL, con);
+        }
+    }
+}
---
+++ Source/DoAnLon/DoAnCNPM/DTO/NhanVienDTO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class NhanVienDTO
+    {
+        #region Các thuộc tính
+        private string _strTenDangNhap;
+        private string _strMatKhau;
+        private string _strHoTen;
+        private string _strChucVu;
+        private string _strDiaChi;
+        private string _strDienThoai;
+        private string _strCMND;
+        #endregion
+
+        #region Các phương thức
+        public string StrTenDangNhap
+        {
+            get { return _strTenDangNhap; }
+            set { _strTenDangNhap = value; }
+        }
+
+        public string StrMatKhau
+        {
+            get { return _strMatKhau; }
+            set { _strMatKhau = value; }
+        }
+
+        public string StrHoTen
+        {
+            get { return _strHoTen; }
+            set { _strHoTen = value; }
+        }
+
+        public string StrChucVu
+        {
+            get { return _strChucVu; }
+            set { _strChucVu = value; }
+        }
+
+        public string StrDiaChi
+        {
+            get { return _strDiaChi; }
+            set { _strDiaChi = value; }
+        }
+
+        public string StrDienThoai
+        {
+            get { return _strDienThoai; }
+            set { _strDienThoai = value; }
+        }
+
+        public string StrCMND
+        {
+            get { return _strCMND; }
+            set { _strCMND = value; }
+        }
+        #endregion
+    }
+}
