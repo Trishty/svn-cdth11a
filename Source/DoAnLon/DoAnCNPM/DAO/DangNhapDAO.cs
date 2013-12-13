a42d3bd5530a9b960844347c34d566ee118e9a0d
EternalUnlimitedTears@gmail.com <EternalUnlimitedTears@gmail.com@fdf90f2a-8789-9f7b-c1d0-b6fda4332c5d>
2013-12-13 16:15:30 +0000

    Sửa CSDL, xóa rác..

--- Source/DoAnLon/DoAnCNPM/DAO/DangNhapDAO.cs
+++ Source/DoAnLon/DoAnCNPM/DAO/DangNhapDAO.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using DTO;
-using System.Data.OleDb;
 using System.Data.SqlClient;
 
 namespace DAO
--- Source/DoAnLon/DoAnCNPM/DAO/LapBaoCaoDoanhThuDAO.cs
+++ Source/DoAnLon/DoAnCNPM/DAO/LapBaoCaoDoanhThuDAO.cs
@@ -4,7 +4,6 @@ using System.Text;
 using DTO;
 using System.Data;
 using System.Data.SqlClient;
-using System.Data.OleDb;
 
 namespace DAO
 {
